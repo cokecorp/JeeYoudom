7ef30046df5a991adc896f815676b4947fc01225
Thibaut <30575266+trmaud@users.noreply.github.com>
2020-04-13 18:14:45 +0200

    First Push

---
+++ core/class/jeeyoudom.class.php
@@ -0,0 +1,167 @@
+<?php
+
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* * ***************************Includes********************************* */
+require_once __DIR__  . '/../../../../core/php/core.inc.php';
+
+class jeeyoudom extends eqLogic {
+    /*     * *************************Attributs****************************** */
+
+
+
+    /*     * ***********************Methode static*************************** */
+
+    /*
+     * Fonction exécutée automatiquement toutes les minutes par Jeedom
+      public static function cron() {
+
+      }
+     */
+
+
+    /*
+     * Fonction exécutée automatiquement toutes les heures par Jeedom
+      */
+     public static function cronHourly() {
+
+        require_once("magpierss/rss_fetch.inc");
+        
+        // Flux RSS source
+        $url_feed = "https://www.pcastuces.com/xml/pca_logitheque.xml";
+        
+        // Nombre d'éléments à afficher
+        $nb_items_affiches=5;
+        
+        // Lecture du flux distant
+        $rss = fetch_rss($url_feed);
+        
+        // Lecture des éléments
+        if (is_array($rss->items))
+        {
+        // On récupère les éléments les + récents
+        $items = array_slice($rss->items,
+        0, $nb_items_affiches);
+        
+        // Début de la liste
+        $html = "<ul>\n";
+        
+        // Boucle sur tous les éléments
+        foreach ($items as $item)
+        {
+        $html .= "<li>";
+        $html .= "<a href=\"".$item['link']."\">";
+        $html .= $item['title']."</a></li>\n";
+        }
+        $html .= "</ul>\n";
+        }
+        
+        // Affiche le code HTML
+        echo $html;
+        
+      }
+     
+
+    /*
+     * Fonction exécutée automatiquement tous les jours par Jeedom
+      public static function cronDaily() {
+
+      }
+     */
+
+
+
+    /*     * *********************Méthodes d'instance************************* */
+
+    public function preInsert() {
+        
+    }
+
+    public function postInsert() {
+        
+    }
+
+    public function preSave() {
+        
+    }
+
+    public function postSave() {
+        
+    }
+
+    public function preUpdate() {
+        
+    }
+
+    public function postUpdate() {
+        
+    }
+
+    public function preRemove() {
+        
+    }
+
+    public function postRemove() {
+        
+    }
+
+    /*
+     * Non obligatoire mais permet de modifier l'affichage du widget si vous en avez besoin
+      public function toHtml($_version = 'dashboard') {
+
+      }
+     */
+
+    /*
+     * Non obligatoire mais ca permet de déclencher une action après modification de variable de configuration
+    public static function postConfig_<Variable>() {
+    }
+     */
+
+    /*
+     * Non obligatoire mais ca permet de déclencher une action avant modification de variable de configuration
+    public static function preConfig_<Variable>() {
+    }
+     */
+
+    /*     * **********************Getteur Setteur*************************** */
+}
+
+class jeeyoudomCmd extends cmd {
+    /*     * *************************Attributs****************************** */
+
+
+    /*     * ***********************Methode static*************************** */
+
+
+    /*     * *********************Methode d'instance************************* */
+
+    /*
+     * Non obligatoire permet de demander de ne pas supprimer les commandes même si elles ne sont pas dans la nouvelle configuration de l'équipement envoyé en JS
+      public function dontRemoveCmd() {
+      return true;
+      }
+     */
+
+    public function execute($_options = array()) {
+        
+    }
+
+    /*     * **********************Getteur Setteur*************************** */
+}
+
+
